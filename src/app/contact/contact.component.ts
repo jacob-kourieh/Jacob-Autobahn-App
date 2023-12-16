db080a933c3ff8f7e5cb99fd1d2d18a5a0910211
Jacob Kourieh <jacob.kourieh@gmail.com>
2023-12-16 17:13:09 +0100

    Implementation About and Contact Features,#16

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import emailjs from 'emailjs-com';
+import { emailJsConfig } from '../../environments/emailjs-config';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
+  isSendingEmail = false;
 
+  constructor() {
+    emailjs.init(emailJsConfig.user_id);
+  }
+
+  sendEmail(event: Event) {
+    event.preventDefault();
+    this.isSendingEmail = true;
+    const form = event.target as HTMLFormElement;
+
+    emailjs
+      .sendForm(emailJsConfig.service_id, emailJsConfig.template_id, form)
+      .then(
+        () => {
+          alert('Email successfully sent!');
+          form.reset();
+        },
+        (err) => {
+          alert('Failed to send email. Error: ' + JSON.stringify(err));
+        }
+      )
+      .finally(() => {
+        this.isSendingEmail = false;
+      });
+  }
 }
